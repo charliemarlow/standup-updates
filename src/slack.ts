70cf4baa019962a39b93cd74141334cbac2478b9
Charlie Marlow <charles.w.marlow@gmail.com>
2025-02-01 13:27:08 -0800

    feat: tag user in thread

--- src/slack.ts
+++ src/slack.ts
@@ -4,11 +4,18 @@ import { config } from "./config";
 const slack = new WebClient(config.slackToken);
 
 export const postMessage = async (message: string) => {
-  // append the slack user to the message
-  const messageWithUser = `${message}\n\n cc <@${config.slackUser}>`;
+  const response = await slack.chat.postMessage({
+    channel: config.slackChannel,
+    text: message,
+    unfurl_links: false,
+    unfurl_media: false,
+  });
+
+  // Add a thread reply with the user mention
   await slack.chat.postMessage({
     channel: config.slackChannel,
-    text: messageWithUser,
+    thread_ts: response.ts,
+    text: `<@${config.slackUser}>`,
     unfurl_links: false,
     unfurl_media: false,
   });
